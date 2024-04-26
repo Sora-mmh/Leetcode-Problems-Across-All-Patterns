8ec5701bbb4ea8fa777a3442e65078497bc7b1bf
Sora-mmh <montassarmhamdi80@gmail.com>
2024-04-26 13:20:20 +0200

    rooting oranges

--- graphs/Rotting_Oranges.py
+++ graphs/Rotting_Oranges.py
@@ -4,23 +4,20 @@ from typing import List
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        def intersect(l1: List[int], l2: List[int]) -> bool:
-            if len(list(set(l1) & set(l2))) == 0:
-                return False
-            else:
-                return True
-
         m, n = len(grid), len(grid[0])
         q = deque([(i, j) for i in range(m) for j in range(n) if grid[i][j] == 2])
-        last_q = q.copy()
+        fresh_oranges_counter = len(
+            [grid[i][j] for i in range(m) for j in range(n) if grid[i][j] == 1]
+        )
+        contamination = False
         visited = []
-        history = []
         if len(q) == 0:
-            return -1
+            if m * n == (m * n - fresh_oranges_counter):
+                return 0
+            else:
+                return -1
         minutes = 0
         while q:
-            # print("q", q)
-            # print(visited)
             cur_ii, cur_jj = q.popleft()
             visited.append((cur_ii, cur_jj))
             neighbors = [
@@ -28,18 +25,17 @@ class Solution:
                 for ii, jj in [(0, -1), (-1, 0), (0, 1), (1, 0)]
                 if 0 <= cur_ii + ii < m and 0 <= cur_jj + jj < n
             ]
-            history.append(neighbors)
             for ii, jj in neighbors:
                 if grid[ii][jj] == 1:
-                    grid[ii][jj] == 2
+                    grid[ii][jj] = 2
+                    fresh_oranges_counter -= 1
+                    contamination = True
                     if (ii, jj) not in visited and (ii, jj) not in q:
                         q.append((ii, jj))
-                elif grid[ii][jj] == 2:
-                    if grid[cur_ii][cur_jj] == 1:
-                        grid[cur_ii][cur_jj] = 2
-            print("last_q", last_q)
-            print("q", q)
-            if not intersect(last_q, q):
+            if contamination:
                 minutes += 1
-            last_q = q.copy()
-        return minutes
+                contamination = False
+        if fresh_oranges_counter != 0:
+            return -1
+        else:
+            return minutes
