f156230768277ee759b917583efe36cb91722e83
Sora-mmh <montassarmhamdi80@gmail.com>
2024-06-23 13:40:00 +0200

    compute the gas station index that allows a cicular possible road

--- arrays/Gas_Station.py
+++ arrays/Gas_Station.py
@@ -4,28 +4,27 @@ from typing import List
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         n = len(gas)
-        # differences = [gas[i] - cost[i] for i in range(n)]
-        # differences = [i for i,difference in enumerate(differences) if difference >= 0]
-        # gas = [gas[i] for i in differences]
-        # cost = [cost[i] for i in differences]
-        # n = len(gas)
-        for idx1 in range(n):
-            if gas[idx1] != 0:
-                tank = gas[idx1]
-                result = idx1
-                idx2 = idx1 + 1 if idx1 + 1 < n else n - 1 - idx1
-                # print(f"for idx1={idx1}, idx2 starts at {idx2}")
-                # print("current tank", tank)
-                while tank >= cost[idx2 - 1] and idx2 != idx1:
-                    # print("update the tank", tank, cost[idx2 - 1], gas[idx2])
-                    tank += gas[idx2] - cost[idx2 - 1]
-                    idx2 += 1
-                    if idx2 >= n:
-                        idx2 -= n
-                    # print("tank", tank)
-                    # print("next idx2", idx2)
-                # print("out of while loop", idx1, idx2)
-                # print("tank", tank, cost[idx2 - 1])
-                if idx2 == idx1 and tank >= cost[idx2 - 1]:
-                    return idx1
-        return -1
+        if sum(gas) < sum(cost):
+            return -1
+        tank = 0
+        result = 0
+        for idx in range(n):
+            tank += gas[idx] - cost[idx]
+            if tank < 0:
+                tank = 0
+                result = idx + 1
+        return result
+
+        # for idx1 in range(n):
+        #     if gas[idx1] != 0:
+        #         tank = gas[idx1]
+        #         result = idx1
+        #         idx2 = idx1 + 1  if idx1 + 1 < n else n - 1 - idx1
+        #         while tank >=  cost[idx2 - 1] and idx2 != idx1:
+        #             tank +=  gas[idx2] - cost[idx2 - 1]
+        #             idx2 += 1
+        #             if idx2 >= n:
+        #                 idx2 -= n
+        #         if idx2 == idx1 and tank >= cost[idx2 - 1]:
+        #             return idx1
+        # return -1
