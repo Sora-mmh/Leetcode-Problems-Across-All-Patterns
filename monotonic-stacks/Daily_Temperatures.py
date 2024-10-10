10d56bf3aa21cdb8e70b3febf158f387764c4c02
Montassar Mhamdi <159718394+Sora-mmh@users.noreply.github.com>
2024-10-10 11:38:28 +0200

    next greater pattern on monotonic stack

--- monotonic-stacks/Daily_Temperatures.py
+++ monotonic-stacks/Daily_Temperatures.py
@@ -3,15 +3,13 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answers = []
+        stack = []
         n = len(temperatures)
-        for idx in range(n - 1):
-            sub_idx = idx + 1
-            while sub_idx < n and temperatures[sub_idx] <= temperatures[idx]:
-                sub_idx += 1
-            if sub_idx == n and temperatures[sub_idx - 1] <= temperatures[idx]:
-                answers.append(0)
-            else:
-                answers.append(sub_idx - idx)
-        answers.append(0)
-        return answers
+        days = [0] * n
+        for idx in range(n):
+            while stack and temperatures[stack[-1]] < temperatures[idx]:
+                current_t = stack.pop()
+                days[current_t] = idx - current_t
+            stack.append(idx)
+        return days
+
