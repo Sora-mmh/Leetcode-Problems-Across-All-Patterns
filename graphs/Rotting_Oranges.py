6a5f2550e1d8a8ee89206af3d534d1d4b90a8158
Montassar Mhamdi <159718394+Sora-mmh@users.noreply.github.com>
2024-10-21 15:38:39 +0200

    final solution with BFS

--- graphs/Rotting_Oranges.py
+++ graphs/Rotting_Oranges.py
@@ -4,38 +4,29 @@ from typing import List
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        import collections
         m, n = len(grid), len(grid[0])
-        q = deque([(i, j) for i in range(m) for j in range(n) if grid[i][j] == 2])
-        fresh_oranges_counter = len(
-            [grid[i][j] for i in range(m) for j in range(n) if grid[i][j] == 1]
-        )
-        contamination = False
-        visited = []
-        if len(q) == 0:
-            if m * n == (m * n - fresh_oranges_counter):
-                return 0
-            else:
-                return -1
-        minutes = 0
-        while q:
-            cur_ii, cur_jj = q.popleft()
-            visited.append((cur_ii, cur_jj))
-            neighbors = [
-                (cur_ii + ii, cur_jj + jj)
-                for ii, jj in [(0, -1), (-1, 0), (0, 1), (1, 0)]
-                if 0 <= cur_ii + ii < m and 0 <= cur_jj + jj < n
-            ]
-            for ii, jj in neighbors:
-                if grid[ii][jj] == 1:
-                    grid[ii][jj] = 2
-                    fresh_oranges_counter -= 1
-                    contamination = True
-                    if (ii, jj) not in visited and (ii, jj) not in q:
-                        q.append((ii, jj))
-            if contamination:
-                minutes += 1
-                contamination = False
-        if fresh_oranges_counter != 0:
+        queue = collections.deque([])
+        positions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        fresh, minutes = 0, 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    fresh += 1
+                if grid[i][j] == 2:
+                    queue.append([i, j])
+        if fresh == 0:
+            return 0
+        if not queue:
             return -1
-        else:
-            return minutes
+        while queue:
+            for _ in range(len(queue)):
+                current_pos = queue.popleft()
+                for pos in positions:
+                    neighbor = [current_pos[0] + pos[0], current_pos[1] + pos[1]]
+                    if 0 <= neighbor[0] < m and  0 <= neighbor[1] < n and grid[neighbor[0]][neighbor[1]] == 1:
+                            grid[neighbor[0]][neighbor[1]] = 2 
+                            fresh -= 1
+                            queue.append(neighbor)
+            minutes += 1
+        return -1 if fresh != 0 else minutes - 1        
