44963ee51e77328b6e25628357495d7d562629a9
Montassar Mhamdi <159718394+Sora-mmh@users.noreply.github.com>
2024-12-23 22:05:04 +0100

    given an array of strings strs, group the  anagrams  together. You can return the answer in any order

--- hashmaps/Group_Anagrams.py
+++ hashmaps/Group_Anagrams.py
@@ -1,24 +1,15 @@
 from typing import List
 
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        from collections import Counter
+        results = defaultdict(list)
+        for word in strs:
+            results["".join(sorted(word))].append(word)
+        return list(results.values())
 
-        # Solution2
-        def check_anagram(str1: str, str2: str):
-            return Counter(str1) == Counter(str2)
 
-        results = []
-        idx = 0
-        while idx < len(strs) or len(strs) != 0:
-            str1 = strs[idx]
-            anagrams = [str1]
-            strs = strs[idx + 1 :]
-            for idx2, str2 in enumerate(strs):
-                if check_anagram(str1, str2):
-                    anagrams.append(str2)
-            for str2 in anagrams[1:]:
-                strs.remove(str2)
-            results.append(anagrams)
-        return results
+
+
+
+
+        
